b8a1a84630144f0f2ffe1991b0f981cd5e05367d
Arcanum417 <lubos.len@gmail.com>
2017-10-25 18:34:40 +0200

    Version

--- addons/main/script_version.hpp
+++ addons/main/script_version.hpp
@@ -1,4 +1,4 @@
 #define MAJOR 10
 #define MINOR 0
-#define PATCHLVL 4
+#define PATCHLVL 5
 #define BUILD 0
--- mod.cpp
+++ mod.cpp
@@ -1,8 +1,8 @@
-name = "Advanced Combat Environment 10.0.4";
+name = "Advanced Combat Environment 10.0.5";
 picture = "logo_ace3_ca.paa";
 actionName = "GitHub";
 action = "https://github.com/acemod/ACE3";
-description = "ACE3 - Version 10.0.4";
+description = "ACE3 - Version 10.0.5";
 logo = "logo_ace3_ca.paa";
 logoOver = "logo_ace3_ca.paa";
 tooltip = "ACE3";
